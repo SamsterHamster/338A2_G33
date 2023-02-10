3e9a622382ee06213b36dc9a4b0d81e7c70b97c2
Mason <mason.bullock@ucalgary.ca>
2023-02-10 16:31:04 -0700

    add ex2 files

---
+++ ex2-2.py
@@ -0,0 +1,66 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+from urllib.request import urlopen
+import json
+import timeit
+
+sys.setrecursionlimit(20000)
+
+
+def func1(arr, low, high):
+    if low < high:
+        pi = func2(arr, low, high)
+        func1(arr, low, pi-1)
+        func1(arr, pi + 1, high)
+
+def func2(array, start, end):
+    p = array[start]
+    low = start + 1
+    high = end
+    while True:
+        while low <= high and array[high] >= p:
+            high = high - 1
+        while low <= high and array[low] <= p:
+            low = low + 1
+        if low <= high:
+            array[low], array[high] = array[high], array[low]
+        else:
+            break
+    array[start], array[high] = array[high], array[start]
+    return high
+
+
+
+url = "https://raw.githubusercontent.com/ldklab/ensf338w23/main/assignments/assignment2/ex2.json"
+response = urlopen(url)
+data = json.loads(response.read())
+
+def sort_func(data, i):
+    quarter = int(len(data[i])/4)
+    func1(data[i], 0, quarter+1)
+    func1(data[i], quarter-1, quarter*2+1)
+    func1(data[i], quarter*2-1, quarter*3+1)
+    func1(data[i], quarter*3-1, len(data[i])-1)
+    
+times = []
+
+for x in range(len(data)):
+    times.append(timeit.timeit(lambda: sort_func(data, x), number=1))
+    x += 1
+ 
+# data to be plotted
+x = []
+for i in range(1000, 10001, 1000):
+    x.append(i)
+    i+=1
+
+
+x = np.array(x)
+y = np.array(times)
+
+# plotting
+plt.xlabel("n")
+plt.ylabel("time (s)")
+plt.plot(x, y, color ="red")
+plt.show()
---
+++ ex2-4.py
@@ -0,0 +1,61 @@
+import json
+import sys
+import timeit
+from urllib.request import urlopen
+import numpy as np
+import matplotlib.pyplot as plt
+
+sys.setrecursionlimit(20000)
+
+def func1(arr, low, high):
+    stack = []
+    stack.append((low, high))
+    while stack:
+        low, high = stack.pop()
+        if low < high:
+            pi = func2(arr, low, high)
+            stack.append((low, pi-1))
+            stack.append((pi+1, high))
+
+def func2(arr, start, end):
+    p = arr[start]
+    low = start + 1
+    high = end
+    while True:
+        while low <= high and arr[high] >= p:
+            high -= 1
+        while low <= high and arr[low] <= p:
+            low += 1
+        if low <= high:
+            arr[low], arr[high] = arr[high], arr[low]
+        else:
+            break
+    arr[start], arr[high] = arr[high], arr[start]
+    return high
+
+url = "https://raw.githubusercontent.com/ldklab/ensf338w23/main/assignments/assignment2/ex2.json"
+response = urlopen(url)
+data = json.loads(response.read())
+
+times = []
+for x in range(len(data)):
+    times.append(timeit.timeit(lambda: func1(data[x], 0, len(data[x])-1), number=1))
+    x += 1
+
+#print(times)
+
+
+# data to be plotted
+x = []
+for i in range(1000, 10001, 1000):
+    x.append(i)
+    i+=1
+
+x = np.array(x)
+y = np.array(times)
+
+# plotting
+plt.xlabel("n")
+plt.ylabel("time (s)")
+plt.plot(x, y, color ="red", label="fuck")
+plt.show()
